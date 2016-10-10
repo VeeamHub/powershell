3e0230241961f8b5032cc162211fcc735932ef91
tdewin <timothy.dewin@gmail.com>
2016-10-10 17:48:59 +0200

    Make-VeeamWindowsScheduledTask
    Get ready to do some custom scheduling

---
+++ Make-VeeamWindowsScheduledTask/Make-VeeamWindowsScheduledTask.ps1
@@ -0,0 +1,124 @@
+﻿[CmdletBinding(PositionalBinding=$false)]
+param(
+    [Parameter(Mandatory=$true)][string]$jobname,
+    [System.DateTime]$At=(get-date),
+    [System.DayOfWeek[]]$days=@(),
+    [string]$scriptpath=("{0}\Veeam\ScheduleScripts" -f $(Get-Item -Path Env:\ProgramData).Value),
+    [string]$scriptfile=("{0}.ps1" -f ($jobname -replace [regex]"[^a-zA-Z0-9]+","_")),
+    [string]$schedulename=("VeeamSchedule_{0}" -f ($jobname -replace [regex]"[^a-zA-Z0-9]+","_")),
+    [string]$schedulepath="\VeeamSchedules\",
+    [System.Management.Automation.PSCredential]$cred = (Get-Credential -Message "Task Credentials" -username $([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)),
+    [switch]$force=$false
+    
+)
+
+
+
+function Make-Scheduler {
+    [CmdletBinding(PositionalBinding=$false)]
+    param(
+        [string]$jobname,
+        [System.DateTime]$at,
+        [System.DayOfWeek[]]$days,
+        [string]$scriptpath=("{0}\Veeam\ScheduleScripts" -f $(Get-Item -Path Env:\ProgramData).Value),
+        [string]$scriptfile=("{0}.ps1" -f ($jobname -replace [regex]"[^a-zA-Z0-9]+","_")),
+        [string]$schedulename=("veeamsched_{0}" -f ($jobname -replace [regex]"[^a-zA-Z0-9]+","_")),
+        [string]$schedulepath="\VeeamSchedules\",
+        [boolean]$force=$false,
+        [System.Management.Automation.PSCredential]$cred
+    )
+
+    #making a directory for holding scripts
+    if (-not (Test-Path -Path $scriptpath)) {
+        $dir = New-Item -Path $scriptpath -ItemType Directory -ErrorAction SilentlyContinue
+        if( -not (Test-Path -Path $scriptpath)) {
+            throw "Subdir creation failed"
+        } else {
+            Write-Verbose "Made Directory $dir"
+        }
+ 
+    } elseif ((Test-Path -Path $scriptpath -PathType Leaf)) {
+        throw "Script dir is already a file"
+    } 
+
+    #check if file already exists
+    $fullscriptpath = Join-Path -Path $scriptpath -ChildPath $scriptfile
+
+    if((Test-path -path $fullscriptpath)) {
+        if (-not $force) {
+            throw "File already exists $fullscriptpath"
+        } 
+    }
+
+
+
+    #check if schedule already exists
+    $schedtest = $(Get-ScheduledTask -TaskName $schedulename -TaskPath $schedulepath -ErrorAction SilentlyContinue)
+    if(($schedtest -ne $null)) {
+        if (-not $force) {
+            throw "Schedule already exists $schedulename"
+        } else {
+            $schedtest | Unregister-ScheduledTask -Confirm:$False
+            Write-Verbose "Removed $schedulename"
+        }
+    }
+
+    $trigger = $null
+
+    if($days.Count -gt 0) {
+        $trigger = New-ScheduledTaskTrigger -At $at -DaysOfWeek $days -Weekly
+    } else {
+        $trigger = New-ScheduledTaskTrigger -Daily -At $at
+    }
+
+    $task = New-ScheduledTaskAction -Execute (Get-Command "powershell").Source -Argument "-NonInteractive -File ""$fullscriptpath"""
+    
+    $sb = New-Object -TypeName "System.Text.StringBuilder"
+    [void]$sb.Append(@"
+#
+# Generated Script
+# 
+Add-PSSnapin Veeampssnapin
+`$jobname = "
+"@)
+    [void]$sb.Append($jobname)
+    [void]$sb.Append(@"
+"
+
+# Edit below if you want even more control
+`$sess = Get-VBRJob -Name `$jobname | Start-VBRJob -RunAsync
+"@)
+
+    $sb.ToString() | Out-File -FilePath $fullscriptpath -Force
+    Write-Verbose "Made Script File : $fullscriptpath"
+    
+
+    $schedtask = Register-ScheduledTask -Action $task -TaskName $schedulename -TaskPath $schedulepath -Trigger $trigger -RunLevel Highest -User $cred.UserName -Password ($cred.GetNetworkCredential().Password)
+    Write-Verbose ("Made task {0}" -f $schedtask.TaskName)
+    
+}
+
+Add-PSSnapin Veeampssnapin
+$j = Get-VBRJob -Name $jobname -ErrorAction SilentlyContinue
+if ($j -eq $null) {
+    throw "Job not found or snapin not loaded"
+}
+
+Make-Scheduler -jobname $jobname -At $at -Days $days -scriptpath $scriptpath -scriptfile $scriptfile -schedulename $schedulename  -schedulepath $schedulepath -force $force -cred $cred
+
+<#
+ # Mass Clean-Up
+
+ #Default Directory
+ $scriptpath=("{0}\Veeam\ScheduleScripts" -f $(Get-Item -Path Env:\ProgramData).Value)
+ Remove-Item -Recurse -Confirm:$True ("{0}\*" -f $scriptpath)
+ 
+
+ #Deleting default schedules
+ [string]$schedulepre=("VeeamSchedule_{0}")
+ [string]$schedulepath="\VeeamSchedules\"
+ $tasks = @(Get-ScheduledTask -TaskPath $schedulepath -ErrorAction SilentlyContinue | ? { $_.TaskName -match $schedulepre })
+ if ($tasks.Count -gt 0) {
+  $tasks | Unregister-ScheduledTask -Confirm:$True
+ }
+#>
---
+++ Make-VeeamWindowsScheduledTask/README.md
@@ -0,0 +1,76 @@
+# Make Veem Windows Scheduled Task
+## VeeamHub
+Veeamhub projects are community driven projects, and are not created by Veeam R&D nor validated by Veeam Q&A. They are maintained by community members which might be or not be Veeam employees.
+
+## Distributed under MIT license
+Copyright (c) 2016 VeeamHub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Project Notes
+**Author:** Timothy Dewin @tdewin
+
+**Function:** Will create a powershell script file under %programdata%\ScheduleScripts and makes a Windows task that will run the powershell script. Removes the burden of figuring out how to start Powershell scripts from the task scheduler. You can edit the schedulescripts if you want to make  special scheduling requirements e.g.
+
+**Special Schedules Examples:**
+Jobname is inserted as static text in the variable $jobname at the top (not via the task scheduler). Thus every job has it's unique script file. If you want to make a customization for every job, you can easily modify this script and change the content while generating the scripts.
+
+*Regular Run (will be generated in the script file by default)*
+```powershell  
+$sess = Get-VBRJob -Name $jobname | Start-VBRJob -RunAsync
+```
+
+*Specific Day In The Month*
+```powershell   
+$now = (get-date)
+if ($now.Day -eq 10) {
+    $sess = Get-VBRJob -Name $jobname | Start-VBRJob -RunAsync
+}
+```
+
+*Full Backup every 2 weeks (alternating)*
+```powershell  
+$week = (get-date -UFormat %V)
+#You can of course skip week check if you run the script only on Friday (-DayOfWeek Friday)
+if ((get-date).DayOfWeek -eq [System.DayOfWeek]::Friday -and ($week%2) -eq 1) {
+    $sess = Get-VBRJob -Name $jobname | Start-VBRJob -RunAsync -FullBackup
+}
+```
+
+
+**Example:**
+
+![Script working](./Media/Screen.png)
+
+**Requires:** Veeam Backup & Replication v9
+
+**Usage:**
+
+Make-VeeamWindowsScheduledTask.ps1 -jobname "jobname" -at "22:00" -verbose -force
+
+**Parameters:**
+
+* -Jobname
+	* Name of job
+* -At
+	* What hour every Day
+
+*More Optional parameters*
+* -force
+  * Overwrites existing script/schedule
+* -verbose
+	* Outputs where the script file was made
+* -DayOfWeek
+	* Allows you to pass a parameter which defines when days to run
+* -scriptpath
+	* <Programdata>\Veeam\ScheduleScripts
+* -scriptfile
+	* Job dervied name
+* -schedulename
+	* VeeamSchedule_<job derived>
+* -schedulepath
+	* Folder in the task scheduler (For group schedules)
