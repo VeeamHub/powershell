81374801053d6ce088a092aa32ccb7b38b1ac63f
nielsengelen <niels@foonet.be>
2018-12-27 09:53:47 +0100

    Update Slack-VM.ps1

--- VAO-Slack-Notification/Slack-VM.ps1
+++ VAO-Slack-Notification/Slack-VM.ps1
@@ -1,8 +1,8 @@
 ﻿param([string]$VMName, [string]$VMIP, [string]$VMIPReplica)
 
 Import-Module PSSlack
-$Uri = "https://hooks.slack.com/services/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"
-$Channel = "vao-test"
+$Uri = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+$Channel = "CHANNELNAME"
 
 $Message = '' + $VMName + ' with IP ' + $VMIP + ' and Replica IP ' + $VMIPReplica + ' has started a DR'
 
