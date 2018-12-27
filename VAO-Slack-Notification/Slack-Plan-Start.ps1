a60a6c0735832406aafa2c415f2e5e68cbde75d9
nielsengelen <niels@foonet.be>
2018-12-27 09:52:55 +0100

    Update Slack-Plan-Start.ps1

--- VAO-Slack-Notification/Slack-Plan-Start.ps1
+++ VAO-Slack-Notification/Slack-Plan-Start.ps1
@@ -1,8 +1,8 @@
 ﻿param([string]$PlanName, [string]$PlanState)
 
 Import-Module PSSlack
-$Uri = "https://hooks.slack.com/services/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"
-$Channel = "vao-test"
+$Uri = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+$Channel = "YOURCHANNEL"
 
 [string]$Message = "+----------------------------------------------------------------------------+"
 $Message += "" | Out-String
