6d37cf77e1b7a040381fdfca142b2294f74e102b
Michael Cade <michaelcade@Michaels-iMac.lan>
2019-01-28 22:02:23 +0000

    Added_MC_VeeamDataLabs
    Adding Secure and Staged Restore Sample Scripts.

---
+++ BR-DataLabs/EntireVMRecovery_StagedRestore.ps1
@@ -0,0 +1,16 @@
+﻿$point = Get-VBRBackup -name "Backup Only Job" | Get-VBRRestorePoint -Name "TPM04-MGMT-01" | Sort-Object –Property CreationTime –Descending | Select -First 1
+$server= Get-VBRServer -Name tpm01-112.aperaturelabs.biz
+$rp = Find-VBRViEntity -Name TPM04-MC
+$ds = Find-VBRViDatastore -server $server -name "SolidFireSWING002"
+$folder = Find-VBRViFolder -server $server -name "TPM04-MC"
+
+$cred = Get-VBRCredentials -name "TPM04-MGMT-01\administrator"
+$vlab = Get-VSBVirtualLab -Name "TPM04-DataLab-03"
+$options = $options = New-VBRApplicationGroupStartupOptions -MaximumBootTime 100 -ApplicationInitializationTimeout 100 -MemoryAllocationPercent 200
+$appgroup = Get-VSBApplicationGroup
+
+#Start-VBRRestoreVM -RestorePoint $point -Server $server -ResourcePool $rp -Datastore $ds -Folder $folder -VMName "TPM04-MGMT-01-NoScriptRestore" -Reason "Normal Entire VM Recovery"
+
+Start-VBRRestoreVM -RestorePoint $point -Server $server -ResourcePool $rp -Datastore $ds -Folder $folder -VMName "TPM04-MGMT-01-NoAppGroup" -Reason "Staged Restore Demo No Application Group Required" -EnableStagedRestore -StagingScript "C:\Users\admin-michaelcade\Desktop\Simple_StagedRestoreScript.ps1" -StagingCredentials $cred -StagingVirtualLab $vlab -StagingStartupOptions $options
+
+#Start-VBRRestoreVM -RestorePoint $point -Server $server -ResourcePool $rp -Datastore $ds -Folder $folder -VMName "TPM04-MGMT-01_AppGroupEnabled" -diskType Thin -Reason "Staged Restore Demo Application Group Required" -EnableStagedRestore -StagingScript "C:\Users\admin-michaelcade\Desktop\Simple_StagedRestoreScript.ps" -StagingCredentials $cred -StagingVirtualLab $vlab -StagingApplicationGroup $appgroup -StagingStartupOptions $options
---
+++ BR-DataLabs/README.md
@@ -0,0 +1,46 @@
+# VeeamDataLabs
+This Repo will contain scripts that enable VeeamDataLabs
+
+IVMR_SecureRestore.ps1 - This script is an example of how to run an Instant VM Recovery process 
+
+EntireVMRecovery_StagedRestore.PS1 - This is an example of a script to invoke an entire vm recovery, note that this script calls another script that will need to present on the local VBR server. 
+
+## Distributed under MIT 
+
+license
+
+Copyright (c) 2018 VeeamHub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+
+associated documentation files (the "Software"), to deal in the Software without restriction, 
+
+including without limitation
+
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+
+so, subject to the following conditions:
+
+The above copyright notice and this permiss
+
+ion notice shall be included in all copies or substantial 
+
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+
+PARTICULAR
+
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+
+THE SOFTWARE OR THE USE OR
+
+OTHER DEALINGS IN THE SOFTWAR
