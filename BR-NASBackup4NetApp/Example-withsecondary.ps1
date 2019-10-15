c7f1ac347e875100b158b566497110040ae9160f
Marco Horstmann <marco.horstmann@veeam.com>
2019-10-15 15:26:43 +0200

    Added NAS Backup 4 NetApp Script

---
+++ BR-NASBackup4NetApp/Example-withsecondary.ps1
@@ -0,0 +1,54 @@
+﻿<#
+   .SYNOPSIS
+   Splatting script to easier read the command parameter of Invoke-NASBackup
+   .Notes 
+   Version:        1.0
+   Author:         Marco Horstmann (marco.horstmann@veeam.com)
+   Creation Date:  15 October 2019
+   Purpose/Change: Reworked documentation and commenting of code.
+   
+   .LINK https://github.com/veeamhub/powershell
+   .LINK https://horstmann.in
+#>
+
+# Please enter below all data which are needed to process snapshots.
+
+$arguments = @{
+   #
+   # Enter here the name or ip adress of the cluster where the share is located.
+   #
+   PrimaryCluster = "192.168.1.220"
+   #
+   # Enter here the name of the SVM which sharing files.
+   #
+   PrimarySVM = "lab-netapp94-svm1"
+   #
+   # Enter here the name of the share you want to snapshot.
+   #
+   PrimaryShare = "vol_cifs"
+   #
+   # Enter the filename of the XML file which was created to store credentials for the primary storage system.
+   #
+   PrimaryClusterCredentials = "C:\scripts\saved_credentials_Administrator.xml"
+   #
+   # Enter here the name or ip adress of the secondary cluster where the backup share is located.
+   #
+   SecondaryCluster = "192.168.1.225"
+   #
+   # Enter here the name of the SVM which has the backup share.
+   #
+   SecondarySVM = "lab-netapp94-svm2"
+   #
+   # Enter here the name of the share you want to use as alternative data path.
+   #
+   SecondaryShare = "vol_cifs_vault" 
+   #
+   # Enter the filename of the XML file which was created to store credentials for the secondary storage system.
+   #
+   SecondaryCredentials = "C:\scripts\saved_credentials_Administrator.xml"
+}
+# 
+
+
+& $PSScriptRoot\Invoke-NASBackup.ps1 @arguments -UseSecondaryDestination
+
---
+++ BR-NASBackup4NetApp/Save-Credential.ps1
@@ -0,0 +1,44 @@
+﻿<# 
+   .SYNOPSIS
+   Save credentials securely into a xl file
+   
+   .DESCRIPTION
+   Save credentials securely into a file for later reuse in e.g. scripts by loading the credentials.
+   Because encryption is done for the current user the credentials can only decrypted by the current user.
+
+   .INPUTS
+   None. You cannot pipe objects to this script
+
+   .EXAMPLE
+   Run for the current user this script and save credentials to a file.
+
+   PS> Save-Credentials.ps1
+
+   The file named "credential.xml" is located in "C:\scripts\"
+ 
+   .EXAMPLE
+   Sometimes it is needed that later a powershell script which is
+   running in "LOCAL SYSTEM" context to use such Credentials. For this
+   we need to run this script in "LOCAL SYSTEM" security context.
+
+   How we do it.
+   1) Install psexec from https://docs.microsoft.com/en-us/sysinternals/downloads/psexec
+   2) Start a powershell with psexec to run script in "LOCAL SYSTEM" security context:
+      C:\Users\Administrator\Downloads\PSTools>PsExec64.exe -i -s powershell.exe -ExecutionPolicy Unrestricted -Command "& '<Path to script>\Save-Credential.ps1' "
+   3) Move c:\scripts\credentials.xml to your folder to save such files.
+
+   .Notes 
+   Version:        1.0
+   Author:         Marco Horstmann (marco.horstmann@veeam.com)
+   Creation Date:  08.10.2019
+   Purpose/Change: Initial script development
+   
+   .LINK https://github.com/VeeamHub/powershell/
+   .LINK https://horstmann.in
+ #>
+
+$credential = Get-Credential
+
+New-Item -Path "c:\" -Name "scripts" -ItemType "directory" -ErrorAction SilentlyContinue
+
+$credential | Export-CliXml -Path "C:\scripts\credential.xml"
