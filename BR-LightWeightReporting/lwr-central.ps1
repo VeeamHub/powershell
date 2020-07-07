db164ee55894fd7bd3e25ad0f77a8399a9d213c6
tdewin <tdewin@users.noreply.github.com>
2020-07-07 18:00:51 +0200

    lightweightreporting
    initial test commit for backup

---
+++ BR-LightWeightReporting/lwr-central.ps1
@@ -0,0 +1,83 @@
+﻿[cmdletbinding()]
+param(
+$lwrpath="c:\veeamlwr",
+$uniqueidfilter="",
+$runtime=(get-date),
+$startinterval=$runtime.AddDays(-1),
+$stopinterval=$runtime,
+[ValidateSet("licensequery","jobquery")]$mode="jobquery"
+)
+
+class LightWeightReportingFile {
+    [string]$fullpath
+    [string]$name
+    [string]$uniqueid
+    [datetime]$date
+    LightWeightReportingFile($fullpath,$name,$uniqueid,$date) {
+        $this.fullpath = $fullpath
+        $this.name = $name
+        $this.uniqueid = $uniqueid
+        $this.date = $date
+    }
+}
+
+
+
+if (-not (Test-Path -Path $lwrpath -PathType Container)) {
+    Write-Verbose "$lwrpath does not exist, creating, no data yet"
+    New-Item -Path $lwrpath -ItemType Directory | out-null
+} else {
+    Write-Verbose "$lwrpath exists, looking for files"
+    Write-Verbose "start time $startinterval"
+    Write-Verbose "stop time $stopinterval"
+    $files = Get-ChildItem -Filter "*.lwr" -Path $lwrpath
+    $parsedFiles = @()
+    foreach ($file in $files) {
+        if ($file.name -match "^(.*)_([0-9]+_[0-9]+).lwr$") {
+            $uid = $Matches[1]
+            $date = [System.DateTime]::ParseExact($Matches[2],"yyyyMMdd_HHmmss",$null)
+            $parsedFiles += [LightWeightReportingFile]::new($file.FullName,$file.Name,$uid,$date)
+        }
+    }
+    if ($uniqueidfilter -ne "") {
+        $parsedFiles = $parsedFiles | where { $_.uniqueid -eq $uniqueidfilter}
+    }
+    $groups = $parsedFiles | Sort-Object -Property date | Group-Object -Property uniqueid
+
+    $targetfiles = @()
+    foreach($uidgroup in $groups) {
+        Write-Verbose ("Identified with {0}" -f $uidgroup.Name)
+        $sorted = $uidgroup.Group | Sort-Object -Descending -Property date
+        $selected = $sorted[0]
+
+        $datefiltered = $sorted | ? { $_.date -gt  $startinterval -and $_.date -lt $stopinterval }
+        if ($datefiltered.Count -gt 0) {
+            $selected = $datefiltered[0]
+        } else {
+            Write-host -ForegroundColor Red ("Selecting most recent for {0} due to no version in the interval ({1})" -f $uidgroup.Name,$selected.date)
+        }
+        $targetfiles += $selected
+    }
+
+    $parsedDataSet = @() 
+    foreach($tf in $targetfiles) {
+        $parsedData = get-content $tf.fullpath | ConvertFrom-Json
+        $parsedDataSet += $parsedData
+    }
+
+    switch($mode) {
+        "licensequery" {
+            $table = $parsedDataSet | select date,sitename,socketsused,socketsinstalled,instanceused,instanceinstalled
+            $table | ft
+        }
+        "jobquery" {
+            $jobtab = @()
+            foreach($pd in $parsedDataSet) {
+               $jobtab += $pd.jobs | select @{n="Site";e={$pd.sitename}},@{n="UpdateStatus";e={$pd.date}},jobname,currentstatus,lastrun,laststatus
+            }
+            $jobtab | ft
+        }
+        
+    }
+    
+}
---
+++ BR-LightWeightReporting/lwr-sitelocal.ps1
@@ -0,0 +1,99 @@
+﻿#lightweight reporting
+
+[cmdletbinding()]
+param(
+$lwrpath="c:\veeamlwr",
+$uniqueid="99570f44-c050-11ea-b3de-0242ac13000x",
+$sitename="DC3"
+)
+
+
+<#
+ Class definitions
+#>
+class LightWeightJob 
+{
+    [string]$jobname
+    [string]$currentstatus
+    [string]$jobtype
+    [string]$sourcetype
+    [datetime]$lastrun
+    [string]$laststatus
+
+    LightWeightJob() {}
+    LightWeightJob($jobname,$currentstatus,$jobtype,$sourcetype,$lastrun,$laststatus) {
+        $this.jobname = $jobname
+        $this.currentstatus = $currentstatus
+        $this.jobtype = $jobtype
+        $this.sourcetype = $sourcetype
+        $this.lastrun = $lastrun
+        $this.laststatus = $laststatus
+    }
+}
+Class LightWeightReport
+{
+    [DateTime]$date
+    [string]$uniqueid
+    [string]$sitename
+    [int]$socketsinstalled
+    [int]$socketsused
+    [int]$instanceinstalled
+    [int]$instanceused
+    [LightWeightJob[]]$jobs
+
+    LightWeightReport() {
+        $this.jobs = @()
+    }
+    LightWeightReport([DateTime]$date,[string]$uniqueid,[string]$sitename,[int]$socketsinstalled,[int]$socketsused,[int]$instanceinstalled,[int]$instanceused) {
+     $this.date = $date
+     $this.uniqueid = $uniqueid
+     $this.sitename = $sitename
+     $this.socketsinstalled = $socketsinstalled
+     $this.socketsused = $socketsused
+     $this.instanceinstalled = $instanceinstalled
+     $this.instanceused = $instanceused
+     $this.jobs = @()
+    }
+}
+
+Add-PSSnapin VeeamPSSnapin
+
+if (-not (Test-Path -Path $lwrpath -PathType Container)) {
+    Write-Verbose "$lwrpath does not exist, creating"
+    New-Item -Path $lwrpath -ItemType Directory | out-null
+}
+
+
+#use now to have a consistent time over the script
+$now = (Get-Date)
+#use 
+$collectpath = Join-Path -Path $lwrpath -ChildPath ("{0}_{1}.lwr" -f $uniqueid,$now.ToString("yyyyMMdd_HHmmss"))
+
+
+$license = Get-VBRInstalledLicense
+[int]$socketsinstalled = 0
+[int]$socketsused = 0
+[int]$instanceinstalled = 0
+[int]$instanceused = 0
+
+foreach ($sl in $license.SocketLicenseSummary) {
+    $socketsinstalled += $sl.LicensedSocketsNumber
+    $socketsused += $sl.UsedSocketsNumber
+}
+
+foreach ($il in $license.InstanceLicenseSummary) {
+    $instanceinstalled += $il.LicensedInstancesNumber
+    $instanceused += $il.UsedInstancesNumber
+}
+
+$lwr = [LightWeightReport]::new($now,$uniqueid,$sitename,$socketsinstalled,$socketsused,$instanceinstalled,$instanceused)
+
+
+foreach($job in (get-vbrjob)) {
+    $ls = $job.FindLastSession()
+    $lwr.jobs += [LightWeightJob]::new($job.Name,$job.GetLastState(),$job.JobType,$job.SourceType,$ls.EndTimeUTC,$ls.Result)
+}
+
+Write-Verbose "Creating $collectpath"
+$lwr | ConvertTo-Json | Out-File -FilePath $collectpath
+
