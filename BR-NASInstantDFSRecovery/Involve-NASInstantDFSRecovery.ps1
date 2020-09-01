8aed6f1b78e8c151b3296f3121cc6da79aa3c27a
Marco Horstmann <marco.horstmann@veeam.com>
2020-09-01 14:47:40 +0200

    New Instant Recovery with DFS switch
    This commit has a complete working script to start an instant NAS recovery of all shares in a given file backup job.
    After this it will change also the reparse points of DFS to direct users to the recovered shares

--- BR-NASInstantDFSRecovery/Involve-NASInstantDFSRecovery.ps1
+++ BR-NASInstantDFSRecovery/Involve-NASInstantDFSRecovery.ps1
@@ -1,46 +1,29 @@
 ﻿<# 
    .SYNOPSIS
-   Getting Shares behind Reparse Points and add them to a NAS Backup Job
+   Start Instant Recovery for all shares and switch DFS paths
    .DESCRIPTION
-   This script finds the shares behind an DFS namespace structure and adds it to VBR NAS Backup Job. You can configure
-   the folder scan depth 
+   This script finds the shares behind an DFS namespace structure and starts for all
+   shares of a job an Instant NAS Recovery session and changes the DFS path to the path
+   on the mount server. 
    .PARAMETER DfsRoot
    With this parameter you specify the UNC path to scan e.g. "\\fileserver\dfs".
    .PARAMETER VBRJobName
-   This is the existing Job where the detected shares should be added.
-   .PARAMETER ShareCredential
-   Enter the Credentials which should be used. They must be from VBR credentials manager.
-   .PARAMETER CacheRepository
-   Enter the Repository which should be used for Cache.
-   .PARAMETER ExcludeSystems
-   Enter list of excluded servername strings like "*server1*","*server2*,"*server3" to exclude reparse points which are
-   pointing to this UNC paths.
+   Name of the NAS backup job. which should be recovered
    .PARAMETER ScanDepth
    How deep in the subfolder structure the script should scan for reparse points?
    .PARAMETER LogFile
-   You can set your own path for log file from this script. Default filename is "C:\ProgramData\dfsresolver4nasbackup.log"
-
-   .Example
-   .\Add-DFSTargetToNASBackupJob.ps1 -DfsRoot "\\homelab\dfs" -VBRJobName "NAS DFS Test" -ShareCredential "HOMELAB\Administrator" -CacheRepository "Default Backup Repository" -ScanDepth 2
-
-   .Example
-   .\Add-DFSTargetToNASBackupJob.ps1 -DfsRoot "\\homelab\dfs" -VBRJobName "NAS DFS Test" -ShareCredential "HOMELAB\Administrator" -CacheRepository "Default Backup Repository" -ScanDepth 2 -VolumeProcessingMode VSSSnapshot
-
-   .Example
-   .\Add-DFSTargetToNASBackupJob.ps1 -DfsRoot "\\homelab\dfs" -VBRJobName "NAS DFS Test" -ShareCredential "HOMELAB\Administrator" -CacheRepository "Default Backup Repository" -ScanDepth 2 -VolumeProcessingMode VSSSnapshot -ExcludeSystems "*lab-dc01*","*lab-nacifs01*" 
-
+   You can set your own path for log file from this script. Default filename is "C:\ProgramData\dfsrecovery.log"
 
    .Example
    .\Involve-NASInstantDFSRecovery.ps1 -DfsRoot "\\homelab\dfs" -ScanDepth 3 -VBRJobName "DFS NAS Test" -Owner "HOMELAB\Administrator"
 
 
    .Notes 
-   Version:        1.8
+   Version:        1.3
    Author:         Marco Horstmann (marco.horstmann@veeam.com)
-   Creation Date:  16 April 2020
-   Purpose/Change: Bugfix: Disallow ProcessingMode StorageSnapshot because it will not work.
+   Creation Date:  20 August 2020
+   Purpose/Change: Initial Release
    
-   .LINK https://github.com/veeamhub/powershell
    .LINK https://github.com/marcohorstmann/powershell
    .LINK https://horstmann.in
  #> 
@@ -66,10 +49,17 @@ Param(
 
 #PROCESS {
 
+    # Get timestamp for log
+    function Get-TimeStamp
+    {    
+        return "[{0:dd.MM.yyyy} {0:HH:mm:ss}]" -f (Get-Date)
+    }
+
     # This function is used to log status to console and also the given logfilename.
     # Usage: Write-Log -Status [Info, Status, Warning, Error] -Info "This is the text which will be logged
     function Write-Log($Info, $Status)
     {
+        $Info = "$(Get-TimeStamp) $Info"
         switch($Status)
         {
             NewLog {Write-Host $Info -ForegroundColor Green  ; $Info | Out-File -FilePath $LogFile}
@@ -122,33 +112,42 @@ Param(
         [Parameter(Mandatory=$true)]$NASRecoverySession
         )
         
-        Set-DfsnFolderTarget -Path $Path -TargetPath $originalPath -State Offline
-        New-DfsnFolderTarget -Path $Path -TargetPath $NASRecoverySession.SharePath -State Online
+        
+        
 
-        [hashtable]$recoveredShareProperty = @{}
+        #Set the original DFS target link to Offline to prevent user access
+        try {
+            $DisableOriginalPath = Set-DfsnFolderTarget -Path $Path -TargetPath $originalPath -State Offline
+            Write-Log -Info "Set Target Path $originalPath to Offline... DONE" -Status Info
+        } catch  {
+            Write-Log -Info "$_" -Status Error
+            Write-Log -Info "Set Target Path $originalPath to Offline... FAILED" -Status Error
+        }
+
+        #Set the original DFS target link back to Online
+        try {
+            $EnableRecoveryPath = New-DfsnFolderTarget -Path $Path -TargetPath $NASRecoverySession.SharePath -State Online
+            Write-Log -Info "Add Recovery Path $($NASRecoverySession.SharePath) to DFS... DONE" -Status Info
+        } catch  {
+            Write-Log -Info "$_" -Status Error
+            Write-Log -Info "Add Recovery Path $($NASRecoverySession.SharePath) to DFS... FAILED" -Status Error
+        }
+
+        $recoveryPath = $NASRecoverySession.SharePath
+
+        #[hashtable]
+        $recoveredShareProperty = @{}
         $recoveredShareProperty.Add('Path',$Path)
         $recoveredShareProperty.Add('originalPath',$originalPath)
-        $recoveredShareProperty.Add('recoveryPath',$NASRecoverySession.SharePath)
-        #$recoveredShare = New-Object -TypeName psobject -Property $recoveredShareProperty
+        $recoveredShareProperty.Add('recoveryPath',$recoveryPath)
         return $recoveredShareProperty
     }
-
-<# Not used because done in main code
-    function Failback-DfsTarget {
-        param (
-        [Parameter(Mandatory=$true)]$Path,
-        [Parameter(Mandatory=$true)]$originalPath,
-        [Parameter(Mandatory=$true)]$recoveryPath
-        )
-        Set-DfsnFolderTarget -Path $Path -TargetPath $originalPath -State Online
-        Remove-DfsnFolderTarget -Path $Path -TargetPath $recoveryPath -Force:$true
-    }
-#>
 
     # End of Functions
 
     # Main Code starts
-    #
+    
+    # Start a new log file
     Write-Log -Status NewLog -Info "Starting new log file"
   
     # Check if DFS Management Tools are installed
@@ -162,45 +161,103 @@ Param(
             Write-Log -Info "DFS Management Tools was installed... DONE" -Status Info
         } catch  {
             Write-Log -Info "$_" -Status Error
-            Write-Log -Info "Installing DFS Management Tools... FAILED" -Status Error
+            Write-Log -Info "DFS Management Tools installation... FAILED" -Status Error
             exit 99
         }
     }
+
+    # Check if AD Management Tools are installed
+    #ToDo: Use it for Checking username of permissionSet
+    Write-Log -Status Info -Info "Checking if Active Directory Powershell modules are installed ..."
+    if(get-windowsFeature -Name "RSAT-AD-PowerShell" | Where-Object -Property "InstallState" -EQ "Installed") {
+        Write-Log -Status Info -Info "Active Directory Powershell modules are already installed... SKIPPED"
+    } else {
+        Write-Log -Status Status -Info "Active Directory Powershell modules are not installed... INSTALLING..."
+        try {
+            Install-WindowsFeature -Name "RSAT-AD-PowerShell" –IncludeAllSubFeature -Confirm:$false
+            Write-Log -Info "Active Directory Powershell modules was installed... DONE" -Status Info
+        } catch  {
+            Write-Log -Info "$_" -Status Error
+            Write-Log -Info "Active Directory Powershell modules installation... FAILED" -Status Error
+            exit 99
+        }
+    }
+
     # Check if Veeam Module can be loaded
     Write-Log -Status Info -Info "Trying to load Veeam PS Snapins ..."
     try {
         Add-PSSnapin VeeamPSSnapin
-        Write-Log -Info "Veeam PS Snapin loaded" -Status Info
+        Write-Log -Info "Loading Veeam PS Snapin ... SUCCESSS" -Status Info
     } catch  {
         Write-Log -Info "$_" -Status Error
-        Write-Log -Info "Failed to load Veeam PS Snapin" -Status Error
+        Write-Log -Info "Loading Veeam PS Snapin ... FAILED" -Status Error
         exit 99
     }
 
     # Validate parameters: VBRJobName
-    Write-Log -Status Info -Info "Checking VBR Job Name"
+    Write-Log -Status Info -Info "Validating VBR Job Name ..."
     $nasBackupJob = Get-VBRNASBackupJob -name $VBRJobName
     if($nasBackupJob -eq $null) {
-        Write-Log -Info "Failed to find job name" -Status Error
+        Write-Log -Info "Validating VBR Job Name ... FAILED" -Status Error
         exit 99
     } else { 
-        Write-Log -Info "VBR Job Name ... FOUND" -Status Info
+        Write-Log -Info "Validating VBR Job Name ... SUCCESSS" -Status Info
     }
 
+    # Validate parameter: Owner
+    Import-Module ActiveDirectory
+
+    # Split user name and domain to check them
+    $OwnerDetails = $Owner.Split('\\').Split("@")
+
+    # Put the right stuff into the variable by determing which notation was used:
+    # DOMAIN\user or user@domain.int
+    if($Owner -like "*\*")
+    {
+        $usercheckDomain = $Ownerdetails[0].ToString()
+        $usercheckUser = $Ownerdetails[1].ToString()
+    } elseif($Owner -like "*@*")
+    {
+        $usercheckDomain = $Ownerdetails[1].ToString()
+        $usercheckUser = $Ownerdetails[0].ToString()
+    } else {
+        Write-Log -Info "$_" -Status Error
+        Write-Log -Info "Was not able to validate user name!" -Status Error
+        exit 99
+    }
+
+    # Check if domain name is valid
+    try {
+        $Null = Get-ADDomain -Identity $usercheckDomain
+        Write-Log -Info "Checking Owner Domain ... DONE" -Status Info
+    } catch {
+        Write-Log -Info "$_" -Status Error
+        Write-Log -Info "Checking Owner Domain ... FAILED" -Status Error
+        exit 99
+    }
+
+    # Check if domain user is valid
+    if((Get-ADUser -Filter {sAMAccountName -eq $usercheckUser}) -eq $Null)
+    {
+        Write-Log -Info "Checking Owner Username ... FAILED" -Status Error
+        exit 99
+    } else {
+        Write-Log -Info "Checking Owner Username ... DONE" -Status Info
+    }
 
     # Scan for Reparse Points
-    Write-Log -Status Info -Info "Scanning for Reparse Points"
+    Write-Log -Status Info -Info "Scanning for Reparse Points ..."
     try {
         $allreparsepoints = Scan-Folder -path $DfsRoot -currentdepth 1 -maxdepth $ScanDepth
         if( $allreparsepoints.Count -eq 0 ) {
-            Write-Log -Info "Reparse Points ... NOT FOUND" -Status Error
-            exit 
+            Write-Log -Info "Scanning for Reparse Points ... NOT FOUND" -Status Error
+            exit 99
         } else {
-            Write-Log -Info "Reparse Points ... FOUND" -Status Info
+            Write-Log -Info "Scanning for Reparse Points ... FOUND" -Status Info
         }
     } catch  {
         Write-Log -Info "$_" -Status Error
-        Write-Log -Info "Failed to find reparse points" -Status Error
+        Write-Log -Info "Scanning for Reparse Points ... FAILED" -Status Error
         exit 99
     }
 
@@ -214,79 +271,84 @@ Param(
         Write-Log -Info "Failed to find Folder Target Paths" -Status Error
         exit 99
     }
-
-    #show all targetpaths
-    #$allTargetPaths
     
-    #Get NAS Backup Job for resolving the restore points
-    $nasBackup = Get-VBRNASBackup -Name $VBRJobName
-    #$nasBackup
     #Get all restore points of this NASBackup to reduce VBR calls
-    $nasBackupRestorePoints = Get-VBRNASBackupRestorePoint -NASBackup $nasBackup
-    #$nasBackupRestorePoints
+    $nasBackupRestorePoints = Get-VBRNASBackup -Name $VBRJobName | Get-VBRNASBackupRestorePoint
+    
     # Get all shares from this object and write it to a new variable sharesInBackup
     $sharesInBackup = $nasBackupRestorePoints | Select-Object -Property NASServerName -Unique
 
-    <#Write-Log -Info "Before show sharesInBackup"
-    $sharesInBackup
-    Write-log -Info "after show sharesInBackup" #>
-
-    #Array for storing the recovery session
+    #Array for storing the Instant Recovery sessions
     $NASRecoverySessions = @()
 
-    #Array and Objects for reverting this NAS recovery
-    #Create the array we'll add the objects to
+    #Array for storing the recovered NAS shares
     $recoveredNASShares = @()
-    #$recoveredNASShares.Add(Get-ComputerInformation -computerName $computer)) | Out-Null
 
     ForEach($shareInBackup IN $sharesInBackup) {
-        #$shareInBackup
+        
         Write-Log -Info "Getting latest restorepoint for share $($shareInBackup.NASServerName.ToString()) ..." -Status Info
-        $nasBackupRestorePoints | Where-Object -Property NASServerName -eq $shareInBackup.NASServerName | Select-Object -First 1 -OutVariable latestRestorePoint
+        #Getting the last restorepoint for this share
+        #$nasBackupRestorePoints | Where-Object -Property NASServerName -eq $shareInBackup.NASServerName | Select-Object -First 1 -OutVariable latestRestorePoint
+        $latestRestorePoint = $nasBackupRestorePoints | Where-Object -Property NASServerName -eq $shareInBackup.NASServerName | Select-Object -First 1
+
+        #Create a PermissionSet to give access to the owner if some folders have no permissions to inherit from
         $nasInstantRecoveryPermissionSet = New-VBRNASPermissionSet -RestorePoint $latestRestorePoint -Owner $Owner -AllowEveryone
-        #$NASInstantRecoveryMountOptions = New-VBRNASInstantRecoveryMountOptions -MountServerSelectionType Automatic -RestorePoint $latestRestorePoint
-        $NASRecoverySessions += Start-VBRNASInstantRecovery -RestorePoint $latestRestorePoint -Permissions $nasInstantRecoveryPermissionSet
-        $currentFolderTarget = $allTargetPaths | Where-Object -Property TargetPath -eq $shareInBackup.NASServerName
-        #Set-DfsnFolderTarget -Path $currentFolderTarget.Path -TargetPath $currentFolderTarget.TargetPath -State Offline
-        #New-DfsnFolderTarget -Path $currentFolderTarget.Path -TargetPath $NASRecoverySessions[$NASRecoverySessions.Count – 1].SharePath -State Online
-        $currentPath = $currentFolderTarget.Path
-        $currentTargetPath = $currentFolderTarget.TargetPath
-        $currentNASRecoverySession = $NASRecoverySessions[$NASRecoverySessions.Count – 1]
-        #, is importent that the array will not extended, with "," the array will be added to the array
-        #$temporaryNASShare = 
-        $recoveredNASShares += (Switch-DfsTarget -Path $currentFolderTarget.Path -originalPath $currentFolderTarget.TargetPath -NASRecoverySession $NASRecoverySessions[$NASRecoverySessions.Count – 1])
-        #$recoveredNASShares += Switch-DfsTarget -Path $currentPath -originalPath $currentTargetPath -NASRecoverySession $currentNASRecoverySession
-        #Write-Log -Status Info -Info "Loopende"
+
+        #Start the NAS Instant Recovery session fort he current share
+        try {
+            $NASRecoverySessions += Start-VBRNASInstantRecovery -RestorePoint $latestRestorePoint -Permissions $nasInstantRecoveryPermissionSet
+            Write-Log -Info "Mounting of share $($shareInBackup.NASServerName.ToString()) ... DONE" -Status Info
+        } catch  {
+            Write-Log -Info "$_" -Status Error
+            Write-Log -Info "Mounting of share $($shareInBackup.NASServerName.ToString())... FAILED" -Status Error
+            Write-Log -Info "Most likely the provided Owner was not found and error handling" -Status Error
+            Write-Log -Info "is not able to validate this." -Status Error
+            exit 99
+}
+        #Get the currentFolderTarget of DFS where the TargetPath is the current share
+        $currentFolderTarget = $allTargetPaths | Where-Object -Property TargetPath -eq $($shareInBackup.NASServerName.ToString())
+        
+        #Add the information to this variable to failback the DFS targets
+        $recoveredNASShares += Switch-DfsTarget -Path $currentFolderTarget.Path -originalPath $currentFolderTarget.TargetPath -NASRecoverySession $NASRecoverySessions[$NASRecoverySessions.Count – 1]
     }
-    #$NASRecoverySessions | FT
-    
-    $confirmation = Read-Host "Are you Sure You Want To Proceed and Clean up what you did?:"
+   
+    #Use this to pause the script, show how nice it works and after this we can cleanup the instant recovery
+    $confirmation = Read-Host "Please confirm, if Recovery sessions should be stopped and DFS links failback to original links?:"
     if ($confirmation -eq 'y') {
         ForEach($recoveredNASShare IN $recoveredNASShares) {
-            #Write-Host "---"
-            #$recoveredNASShare
-            Write-Log -Info "Das Target ist $($recoveredNASShare.Path)" -Status Info
-            #Write-Host "---"
-        #    Switch-DfsTarget -Path $recoveredNASShare.Path -originalPath $recoveredNASShare.TargetPath -NASRecoverySession $recoveredNASShare.NASRecoverySession -Failback
-            Remove-DfsnFolderTarget -Path $recoveredNASShare.Path -TargetPath $recoveredNASShare.recoveryPath -Force:$true
-            Set-DfsnFolderTarget -Path $recoveredNASShare.Path -TargetPath $recoveredNASShare.originalPath -State Online
+            Write-Log -Info "Failback of $($recoveredNASShare.Path) started" -Status Info
+            
+            #Delete the DFS target link to the Instant Recovery Share
+            try {
+                $null = Remove-DfsnFolderTarget -Path $recoveredNASShare.Path -TargetPath $recoveredNASShare.recoveryPath -Force:$true
+                Write-Log -Info "Removing Recovery Target $($recoveredNASShare.recoveryPath)... DONE" -Status Info
+            } catch  {
+                Write-Log -Info "$_" -Status Error
+                Write-Log -Info "Removing Recovery Target $($recoveredNASShare.recoveryPath)... FAILED" -Status Error
+            }
+            
+            #Set the original DFS target link back to Online
+            try {
+                $null = Set-DfsnFolderTarget -Path $recoveredNASShare.Path -TargetPath $recoveredNASShare.originalPath -State Online
+                Write-Log -Info "Activating previous Target Path $($recoveredNASShare.originalPath)... DONE" -Status Info
+            } catch  {
+                Write-Log -Info "$_" -Status Error
+                Write-Log -Info "Activating previous Target Path $($recoveredNASShare.originalPath)... FAILED" -Status Error
+            }
+            
+            
         }
         
-        <#
-        #Debug Removal
-        Remove-DfsnFolderTarget -Path "\\homelab\dfs\Orga\IT" -TargetPath "\\lab-vbr11\it" -Force:$true
-        Set-DfsnFolderTarget -Path "\\homelab\dfs\Orga\IT" -TargetPath "\\lab-nacifs01\it" -State Online
-        Remove-DfsnFolderTarget -Path "\\homelab\dfs\Field\Sales" -TargetPath "\\lab-vbr11\sales" -Force:$true
-        Set-DfsnFolderTarget -Path "\\homelab\dfs\Field\Sales" -TargetPath "\\lab-nacifs01\sales" -State Online
-        Remove-DfsnFolderTarget -Path "\\homelab\dfs\Field\Marketing" -TargetPath "\\lab-vbr11\marketing" -Force:$true
-        Set-DfsnFolderTarget -Path "\\homelab\dfs\Field\Marketing" -TargetPath "\\lab-nacifs01\marketing" -State Online
-        #>
-
+        #Stop all running Instant Recoveries started by this script
         ForEach($NASRecoverySession IN $NASRecoverySessions) {
-            Stop-VBRNASInstantRecovery -InstantRecovery $NASRecoverySession
+            try {
+                Stop-VBRNASInstantRecovery -InstantRecovery $NASRecoverySession
+                Write-Log -Info "Stopping recovery of $($NASRecoverySession.SessionName)... DONE" -Status Info
+            } catch  {
+                Write-Log -Info "$_" -Status Error
+                Write-Log -Info "Stopping recovery of $($NASRecoverySession.SessionName)... FAILED" -Status Error
+            }
+            
         }
-    }
     
-    # .\Involve-NASInstantDFSRecovery.ps1 -DfsRoot "\\homelab\dfs" -ScanDepth 3 -VBRJobName "DFS NAS Test" -Owner "HOMELAB\Administrator"
-#Stop-VBRNASInstantRecovery -InstantRecovery $NASRecoverySessions
-#} # END PROCESS
+} # END PROCESS
