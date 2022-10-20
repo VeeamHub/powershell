7b320d4c078792619c950a0efa83f5425243ddda
Stefan Zimmermann <stefan.zimmermann@veeam.com>
2022-10-20 23:49:27 +0200

    Rewrite for teams only and incudes & excludes

---
+++ VB365-SPO-Teams-Jobs/README.md
@@ -0,0 +1,43 @@
+# A script to automate the creation and operation of SharePoint and Teams jobs
+
+## Author
+
+* Stefan Zimmermann ([StefanZ8n](https://github.com/StefanZ8n))
+  
+## Function
+
+Create and maintain SharePoint Online and Teams jobs in VB365. 
+The script offers the following major features:
+
+- Creates backup jobs for all processed objects respecing a maximum number of objects per job
+- Round robins through a list of repositories for created jobs
+- Reuses jobs matching the naming scheme which can still hold objects
+- Puts Sharepoint sites and matching teams to the same job
+- Schedules created jobs with a delay to not start all at the same time
+- Can work with include and exclude patterns from files (regex)
+
+## Limitations
+
+* Matching of SPO sites and Teams is based on the SPO URL and the Team's e-mail address. 
+  Both don't change when a team is renamed.
+
+## Known Issues
+
+* New jobs will currently always be created starting with the first repo in the list
+* The base schedule will be delayed for every job in sequence, but not based on which proxy they are configured
+
+## Requirements
+
+All scripts are designed to be executed on the VB365 server.
+
+* Veeam Backup for Microsoft 365 v6
+
+## Usage Examples
+
+```powershell
+./vb365-spo-teams-jobs.ps1 -Organization "my-org.onmicrosoft.com" -Repositories Proxy1-Repo1,Proxy1-Repo2,Proxy2-Repo1,Proxy2-Repo2 -objectsPerJob 200
+```
+
+Will add all SharePoint sites which are currently not in backup to jobs and search for matching Teams during the process to add them to the same job.
+
+
--- VB365-SPO-Teams-Jobs/vb365-spo-teams-jobs.ps1
+++ VB365-SPO-Teams-Jobs/vb365-spo-teams-jobs.ps1
@@ -1,20 +1,52 @@
-<#
-.SYNOPSIS
-    Automatically create jobs for SharePoint Online Sites and corresponding Teams which can be distributed accross
-.DESCRIPTION
-    TO BE DONE
 
-    Requires Veeam.Archiver.PowerShell module on the system.
-.EXAMPLE
-    TO BE DONE
-.INPUTS
-    TO BE DONE
-.OUTPUTS
-    TO BE DONE
-.NOTES
-    Written by Stefan Zimmermann <stefan.zimmermann@veeam.com>
+<#PSScriptInfo
+
+.VERSION 0.2.0
+
+.GUID f3795945-b130-4740-84f4-e8248a847263
+
+.AUTHOR Stefan Zimmermann <stefan.zimmermann@veeam.com>
+
+.COMPANYNAME
+
+.COPYRIGHT
+
+.TAGS Veeam Backup for Microsoft 365
+
+.LICENSEURI https://github.com/VeeamHub/powershell/blob/master/LICENSE
+
+.PROJECTURI https://github.com/VeeamHub/powershell
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+IN DEVELOPMENT
+
+.PRIVATEDATA
+
 #>
-#requires -modules Veeam.Archiver.PowerShell
+
+#Requires -Module Veeam.Archiver.PowerShell
+
+<# 
+
+.DESCRIPTION 
+ Script to automatically create and maintain Sharepoint and Teams backup jobs in Veeam Backup for Microsoft 365.
+ The script delivers the following major features:
+ - Creates backup jobs for all processed objects respecing a maximum number of objects per job
+ - Round robins through a list of repositories for created jobs
+ - Reuses jobs matching the naming scheme which can still hold objects
+ - Puts Sharepoint sites and matching teams to the same job
+ - Schedules created jobs with a delay to not start all at the same time
+ - Can work with include and exclude patterns
+ 
+#> 
 [CmdletBinding()]
 Param(        
     [Parameter()]
@@ -22,21 +54,31 @@ Param(
     # When backing up teams the maximum is this number-1 because teams should be grouped with sites
     [int] $objectsPerJob = 500,
 
-    # Include teams when building jobs
-    [switch] $withTeams = $true,
+    # Limit processed service to either only SharePoint or Teams
+    [String][ValidateSet("SharePoint", "Teams")] $limitServiceTo = $null,
 
     # Format string to build the jobname. {0} will be replaced with the number of the job and can be formatted as PowerShell format string
     # {0:d3} will create a padded number. 2 will become 002, 12 will become 012
     [string] $jobNamePattern = "SharePointTeams-{0:d2}",
 
-    # Include Teams Chats, only used if `withTeams` is $true
+    # Include chats in Teams backups
     [switch] $withTeamsChats = $false,
 
     # Base schedule for 1st job
-    [Object] $baseSchedule,
+    [Object] $baseSchedule = $null,
 
     # Delay between the starttime of two jobs in HH:MM:SS format
-    [string] $scheduleDelay = "00:10:00"
+    [string] $scheduleDelay = "00:30:00",
+
+    # Path to file with patterns to include when building jobs
+    # Includes will be processed before excludes
+    # If not set will try to load a file with the same name as the script and ending ".include"
+    [string] $includeFile = $null,
+
+    # Path to file with patterns to exclude when building jobs (Excludes won't be added to jobs, they won't be excluded)
+    # Excludes will be processed after includes
+    # If not set will try to load a file with the same name as the script and ending ".exclude"
+    [string] $excludeFile = $null
 
 
 )
@@ -47,9 +89,8 @@ DynamicParam {
     # Organizations
     $OrganizationParameter = 'Organization'        
     $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
-    # Change back to true after debugging
-    $ParameterAttribute.Mandatory = $false        
+    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute    
+    $ParameterAttribute.Mandatory = $true
     $AttributeCollection.Add($ParameterAttribute)    
     $arrSet = Get-VBOOrganization | select -ExpandProperty Name
     # Add .onmicrosoft.com name in case organization was renamed
@@ -64,9 +105,8 @@ DynamicParam {
     # Repositories
     $RepositoryParameter = 'Repository'        
     $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]    
-    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
-    # Change back to true after debugging
-    $ParameterAttribute.Mandatory = $false        
+    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute    
+    $ParameterAttribute.Mandatory = $true
     $AttributeCollection.Add($ParameterAttribute)    
     $arrSet = Get-VBORepository | select -ExpandProperty Name    
     $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
@@ -80,28 +120,62 @@ DynamicParam {
 BEGIN {
     $organization = $PsBoundParameters[$OrganizationParameter]
     $repository = $PsBoundParameters[$RepositoryParameter]
-    #$organization = "90d"
-    #$repository = "Local-Item","Local-Snapshot"
 
     if (!$baseSchedule) {        
         $baseSchedule = New-VBOJobSchedulePolicy -EnableSchedule -Type Daily -DailyType Everyday -DailyTime "22:00:00"
     }
 
-    Start-Transcript -IncludeInvocationHeader -Path ".\logs\vb365-spo-teams-jobs-$(get-Date -Format FileDateTime).log" -NoClobber
+    $basename = $MyInvocation.MyCommand.Name.Split(".")[0]
+
+    if (!$includeFile -and (Test-Path -PathType Leaf -Path "${PSScriptRoot}\${basename}.include")) {
+        $includeFile = "${PSScriptRoot}\${basename}.include"        
+    }
+
+    if (!$excludeFile -and (Test-Path -PathType Leaf -Path "${PSScriptRoot}\${basename}.exclude")) {
+        $excludeFile = "${PSScriptRoot}\${basename}.exclude"        
+    }
+
+    
+    if ($includeFile) {
+        $includes = Get-Content $includeFile
+    }
+
+    if ($excludeFile) {
+        $excludes = Get-Content $excludeFile
+    }
+
+    Start-Transcript -IncludeInvocationHeader -Path "${PSScriptRoot}\logs\vb365-spo-teams-jobs-$(get-Date -Format FileDateTime).log" -NoClobber
 }
 
 PROCESS {
     $org = Get-VBOOrganization -Name $organization
-    $repos = $repository | % { Get-VBORepository -Name $_ }
-    # Sort by SiteId to make sure pages are properly distributed
-    $sites = Get-VBOOrganizationSite -Organization $org -NotInJob -WarningAction:SilentlyContinue | sort -Property SiteId
-    "Found {0} sites not yet in jobs" -f $sites.Count
-    
+    $repos = $repository | % { Get-VBORepository -Name $_ }    
+
+    $sites = @()
     $teams = @()
-    if ($withTeams) { 
+
+    if (!$limitServiceTo -or $limitServiceTo -eq "SharePoint") {
+        $sites = Get-VBOOrganizationSite -Organization $org -NotInJob -WarningAction:SilentlyContinue    
+        "Found {0} SharePoint sites not yet in backup jobs" -f $sites.Count
+    }
+
+    if (!$limitServiceTo -or $limitServiceTo -eq "Teams") {
         $teams = Get-VBOOrganizationTeam -NotInJob -Organization $org 
-        "Found {0} Teams not in jobs" -f $teams.Count
-    } 
+        "Found {0} Teams not yet in backup jobs" -f $teams.Count
+    }
+
+    # Iterate through teams only if no SP is selected, otherwise SP is always leading processing
+    # Sort by UUIDs for randomizing and better load balancing
+    $objects = if ($limitServiceTo -eq "Teams") { $teams | sort -Property OfficeId } else { $sites | sort -Property SiteId } 
+    
+
+    if ($includes) {
+        "Adding {0} include patterns to process from {1}" -f $includes.Count,$includeFile
+    }
+    
+    if ($excludes) {
+        "Adding {0} exclude patterns to process from {1}" -f $excludes.Count,$excludeFile
+    }
 
     $jobCounter = 1
     [Veeam.Archiver.PowerShell.Model.VBOJob] $currentJob = $null    
@@ -109,19 +183,39 @@ PROCESS {
     [Veeam.Archiver.PowerShell.Model.VBOJobSchedulePolicy] $lastSchedule = $null
     [System.Collections.Queue] $repoQueue = New-Object System.Collections.Queue    
     
-    $siteCounter = 0
+    $objCounter = 0
     $teamCounter = 0
     $jobTouchedCounter = 0
     $jobCreatedCounter = 0 
 
-    # When to add objects to an existing job
-    $minFreeObjects = if ($withTeams) { 2 } else { 1} 
+    # Adapt minimum free objects in job to stay below maximum when adding SP and Teams
+    $minFreeObjects = if ($noTeams -or $noSharepoint) { 1 } else { 2 } 
 
 
-    # Map Teams to SPO sites to include them in the same job to put them on the same repo
-    foreach ($site in $sites) {
+    foreach ($o in $objects) {
 
-        $bSite = New-VBOBackupItem -Site $site
+        if ($includes) {
+            $include = $includes | Where-Object { $o.toString() -cmatch $_ }
+            if ($include) {
+                "Include {0} because of pattern {1}" -f $o.toString(),$include
+            } else {
+                continue
+            }
+        }
+
+        if ($excludes) {
+            $exclude = $excludes | Where-Object { $o.toString() -cmatch $_ }
+            if ($exclude) {
+                "Exclude {0} because of pattern {1}" -f $o.toString(),$exclude
+                continue
+            }
+        }
+
+        if ($limitServiceTo -eq "Teams") {            
+            $bObject = New-VBOBackupItem -Team $o -TeamsChats:$withTeamsChats                                
+        } else {            
+            $bObject = New-VBOBackupItem -Site $o            
+        }
 
         while (!$currentJob) {
             $jobName = $jobNamePattern -f $jobCounter
@@ -136,7 +230,7 @@ PROCESS {
                 $currentJobObjects = Get-VBOBackupItem -Job $currentJob
                 "Found existing job {0} with {1} objects" -f $currentJob,$currentJobObjects.Count
                 if ($currentJobObjects.Count -gt ($objectsPerJob - $minFreeObjects)) {
-                    Write-Host "Skipping job, object limit already reached (${objectsPerJob})"
+                    "Skipping job, object limit already reached ({0})" -f $objectsPerJob
                     $lastSchedule = $currentJob.SchedulePolicy
                
                     # Won't use this job, search for next
@@ -144,7 +238,7 @@ PROCESS {
                     $jobCounter++
                     continue
                 }
-                Write-Host "Adding objects to job"
+                "Adding objects to job {0}" -f $currentJob
                 $jobTouchedCounter++
                 
             } else {                
@@ -152,25 +246,26 @@ PROCESS {
                     $lastSchedule = $currentSchedule                    
                     $currentSchedule = New-VBOJobSchedulePolicy -EnableSchedule -Type $lastSchedule.Type -DailyType $lastSchedule.DailyType -DailyTime ($lastSchedule.DailyTime+$scheduleDelay)
                 }                
-                $currentJob = Add-VBOJob -Organization $org -Name $jobName -SchedulePolicy $currentSchedule -Repository $repoQueue.Dequeue() -SelectedItems $bSite                
-                Write-Host "No usable job found - created new job $currentJob"
+                $currentJob = Add-VBOJob -Organization $org -Name $jobName -SchedulePolicy $currentSchedule -Repository $repoQueue.Dequeue() -SelectedItems $bObject -Description "Created by vbo-spo-teams-jobs v$($version) on $(Get-Date)"
+                "No usable job found - created new job {0}" -f $currentJob
                 $jobTouchedCounter++
                 $jobCreatedCounter++
             }            
         }
         
-        Add-VBOBackupItem -Job $currentJob -BackupItem $bSite
-        Write-Host "Added SPO site to job ${currentJob}: $site"
-        $siteCounter++
+        Add-VBOBackupItem -Job $currentJob -BackupItem $bObject
+        "Added object to job {0}: {1}" -f $currentJob,$o
+        $objCounter++
         
-        if ($withTeams) {
-            $url = [uri] $site.URL
+        # If any service limit is set either do not process teams (limit to SP) or teams are already processed on $o level (limit to Teams)
+        if (!$limitServiceTo) {
+            $url = [uri] $o.URL
             $urlName = $url.Segments[-1]
             $matchedTeam = $teams | ? { ($_.Mail -split "@")[0] -eq $urlName }
             if ($matchedTeam) {
                 $bTeam = New-VBOBackupItem -Team $matchedTeam -TeamsChats:$withTeamsChats                                
                 Add-VBOBackupItem -Job $currentJob -BackupItem $bTeam
-                Write-Host "Added Team to job ${currentJob}: ${matchedTeam}"
+                "Added matching Team to job {0}: {1}" -f $currentJob,$matchedTeam
                 $teamCounter++
             }
         }
@@ -185,7 +280,10 @@ PROCESS {
         }
     }
 
-    Write-Host "Added ${siteCounter} sites and ${teamCounter} teams to ${jobTouchedCounter} touched and ${jobCreatedCounter} created backup jobs"     
+    $primaryObject = if ($limitServiceTo -eq "Teams") { "Teams" } else { "Sharepoint Sites" }
+    $teamCountText = if (!$limitServiceTo) { "and ${teamCounter} teams " } else { "" }
+
+    Write-Host "Added ${objCounter} ${primaryObject} ${teamCountText}to ${jobTouchedCounter} touched and ${jobCreatedCounter} created backup jobs"     
 
 }
 
