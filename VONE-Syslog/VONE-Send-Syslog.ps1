620d98accda819568e7c171e104dc99c0319794c
Jorge de la Cruz <jorgedlcruz@gmail.com>
2023-03-03 19:01:55 +0000

    VONE to Syslog
    VONE to Syslog

---
+++ VONE-Syslog/VONE-Send-Syslog.ps1
@@ -0,0 +1,8 @@
+﻿param([string]$SyslogServer,[string]$AlarmName,[string]$Summary,[string]$NodeName,[string]$Status,[string]$ID)
+If ($Status -eq "Resolved"){
+    $Status= "Notice"
+    Send-SyslogMessage -Server $($SyslogServer) -Facility 'mail' -ApplicationName "$($NodeName)" -ProcessID "$($ID)" -Message "$($AlarmName), $($Summary)" -Severity "$($Status)"
+}
+else{
+    Send-SyslogMessage -Server $($SyslogServer) -Facility 'mail' -ApplicationName "$($NodeName)" -ProcessID "$($ID)" -Message "$($AlarmName), $($Summary)" -Severity "$($Status)"
+}
