a5266869ffb2c434b195e5f3c968013e4baf0c08
Tyson Fewins <13089078+tfewins@users.noreply.github.com>
2024-10-16 15:54:18 -0400

    Update VMwareProxyAutomation.ps1
    updated match proxy VM logic in CheckVBOProxies function

--- VBO-ProxyAutomation/VMware/VMwareProxyAutomation.ps1
+++ VBO-ProxyAutomation/VMware/VMwareProxyAutomation.ps1
@@ -140,11 +140,16 @@ function CheckVBOProxies ($ProxyPool) {
         $proxy = Get-VBOProxy -Hostname $proxyHostname
         $ProxyVMName = $null
         $ProxyMode = $null
-        foreach ($ProxyVM in $ProxyVMList) {
+        
+        # Match VBO Proxy to Proxy VM
+        foreach ($ProxyVM in $VBOAutomatedProxies) {
+            $match = $false
             if ($proxy.Hostname -eq $ProxyVM.ProxyVMIPv4) {
-                $ProxyVMName = $ProxyVM.ProxyVMName
-                $ProxyMode = $ProxyVM.ProxyMode
+                $match = $true
             } elseif ($proxy.Hostname -eq $ProxyVM.ProxyVMName) {
+                $match = $true
+            }
+            if ($match) {
                 $ProxyVMName = $ProxyVM.ProxyVMName
                 $ProxyMode = $ProxyVM.ProxyMode
             }
