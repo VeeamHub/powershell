a7fb5bcbe8c770556500150f05c3a0c755cdcb81
Chris Arceneaux <carcenea@gmail.com>
2025-12-17 12:53:34 -0500

    Updated script to support Graph API
    - Removed references to deprecated PowerShell modules
    - Replaced with Graph API module and cmdlets

--- VB365-RestorePortalSetup/Connect-VB365RestorePortal.ps1
+++ VB365-RestorePortalSetup/Connect-VB365RestorePortal.ps1
@@ -10,7 +10,7 @@ function Connect-VB365RestorePortal {
 
 .DESCRIPTION
   The script logs in to a tenant Microsoft 365 environment and grants the required permissions so the tenant can leverage a service provider's Veeam Backup for Microsoft 365 Restore Portal.
-	
+
 .PARAMETER ApplicationId
 	Service Provider (Enterprise Application) Application ID. THIS IS PROVIDED BY YOUR SERVICE PROVIDER.
 
@@ -33,25 +33,22 @@ function Connect-VB365RestorePortal {
 
 .NOTES
 	NAME:  Connect-VB365RestorePortal
-	VERSION: 1.0
+	VERSION: 1.2
 	AUTHOR: Chris Arceneaux
 	TWITTER: @chris_arceneaux
 	GITHUB: https://github.com/carceneaux
 
 .LINK
-  https://helpcenter.veeam.com/docs/vbo365/guide/ssp_configuration.html
+  https://helpcenter.veeam.com/docs/vbo365/guide/ssp_configuration.html#on-tenant-side
 
 .LINK
   https://helpcenter.veeam.com/docs/vbo365/guide/ssp_ad_application_permissions.html
 
 .LINK
-  https://docs.microsoft.com/en-us/powershell/module/azuread/new-azureadserviceprincipal
+	https://learn.microsoft.com/en-us/graph/permissions-reference#externalconnectionreadwriteownedby
 
 .LINK
-  https://f12.hu/2021/01/13/grant-admin-consent-to-an-azuread-application-via-powershell/
-
-.LINK
-	https://arsano.ninja/
+  https://learn.microsoft.com/en-us/graph/permissions-reference#externalitemreadwriteownedby
 
 #>
 
@@ -63,28 +60,24 @@ function Connect-VB365RestorePortal {
 
   # connecting to all things Microsoft
   try {
-    Write-Verbose "Connecting to Microsoft Azure account"
-    Connect-AzAccount -ErrorAction Stop | Out-Null
-    $context = Get-AzContext
-    Write-Verbose "Connecting to Azure AD account"
-    Connect-AzureAD -TenantId $context.Tenant.TenantId -AccountId $context.Account.Id -ErrorAction Stop | Out-Null
-    Write-Host "$($context.Account.Id) is now connected to Microsoft Azure" -ForegroundColor Green
+    Write-Verbose "Connecting to Microsoft Graph API"
+    Connect-MgGraph -Scopes "Application.ReadWrite.All","Directory.ReadWrite.All" -NoWelcome
   }
   catch {
-    Write-Error "An issue occurred while logging into Microsoft. Please double-check your credentials and ensure you have sufficient access."
+    Write-Error "An issue occurred while logging into Microsoft. Please double-check your credentials and ensure you have sufficient permissions (Global Administrator OR Application Administrator)."
     throw $_
   }
 
   # check if Enterprise Application already exists
-  $sp = Get-AzureADServicePrincipal -Filter "AppId eq '$ApplicationId'"
+  $sp = Get-MgServicePrincipal -Filter "appId eq '$ApplicationId'"
   if ($sp) {
-    Write-Verbose "Enterprise Application ($ApplicationId) already exists"
+    Write-Verbose "Enterprise Application ($ApplicationId) is already linked to your Entra ID tenant"
   }
   else {
     # creating link to Service Provider Enterprise Application
     try {
       Write-Verbose "Creating new Azure AD Service Principal"
-      $sp = New-AzureADServicePrincipal -AppId $ApplicationId -ErrorAction Stop
+      $sp = New-MgServicePrincipal -AppId $ApplicationId -ErrorAction Stop
       Write-Host "$($sp.DisplayName) ($($sp.AppId)) has been linked your account" -ForegroundColor Green
     }
     catch {
@@ -93,64 +86,29 @@ function Connect-VB365RestorePortal {
     }
   }
 
-  # granting admin consent
-  $token = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($context.Account, $context.Environment, $context.Tenant.TenantId, $null, "Never", $null, "74658136-14ec-4630-ad9b-26e160ff0fc6")
-  $headers = @{
-    'Authorization'          = 'Bearer ' + $token.AccessToken
-    'X-Requested-With'       = 'XMLHttpRequest'
-    'x-ms-client-request-id' = New-Guid
-    'x-ms-correlation-id'    = New-Guid
-  }
-  $url = "https://main.iam.ad.ext.azure.com/api/RegisteredApplications/$($sp.AppId)/Consent?onBehalfOfAll=true"
-  Write-Verbose "Granting admin consent to the newly linked Azure AD Service Principal"
-
-  # loop waiting for change to actually take place
-  while ($true) {
-    try {
-      Invoke-RestMethod -Uri $url -Headers $headers -Method POST -ErrorAction Stop | Out-Null
-      break
-    }
-    catch {
-      Write-Host "Waiting to grant admin consent... (this can take up to 15 minutes)" 
-      Write-Verbose "Error: $_"
-      Start-Sleep -Seconds 5
-    }
-  }
+  # granting admin consent to the Service Provider Enterprise Application
+  # see LINK for reference documentation
+  $graphSpId = $(Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'").Id
+  New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $sp.Id -PrincipalId $sp.Id -AppRoleId "f431331c-49a6-499f-be1c-62af19c34a9d" -ResourceId $graphSpId -ErrorAction Stop | Out-Null
+  New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $sp.Id -PrincipalId $sp.Id -AppRoleId "8116ae0f-55c2-452d-9944-d18420f5b2c8" -ResourceId $graphSpId -ErrorAction Stop | Out-Null
   Write-Host "$($sp.DisplayName) ($($sp.AppId)) has been granted admin consent" -ForegroundColor Green
   Write-Host "You can now login to the Service Provider's VB365 Restore Portal!" -ForegroundColor Green
   Write-Warning "If you receive an error, wait 15 minutes and attempt login again."
 
   # logging out of remote sessions
-  Write-Verbose "Logging out of Azure AD account"
-  Disconnect-AzureAD | Out-Null
-  Write-Verbose "Logging out of Microsoft Azure account"
-  Disconnect-AzAccount | Out-Null
+  Write-Verbose "Logging out of Microsoft Graph API"
+  Disconnect-MgGraph | Out-Null
 }
 
-Write-Host "Installing required Azure PowerShell modules...Az.Accounts & AzureAd"
+Write-Host "Installing required PowerShell module...Microsoft.Graph"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Find-PackageProvider -Name Nuget -ForceBootstrap -IncludeDependencies -Force | Out-Null
-# Determine if Az.Account module is already present
-if ( -not(Get-Module -ListAvailable -Name Az.Accounts)){
-  Install-Module -Name Az.Accounts -SkipPublisherCheck -Force -ErrorAction Stop
-  Write-Host "Az.Accounts module installed successfully" -ForegroundColor Green
+# Determine if Microsoft.Graph module is already present
+if ( -not(Get-Module -ListAvailable -Name Microsoft.Graph)){
+  Install-Module -Name Microsoft.Graph -SkipPublisherCheck -Force -ErrorAction Stop
+  Write-Host "Microsoft.Graph module installed successfully" -ForegroundColor Green
 } else {
-  Write-Host "Az.Accounts module already present" -ForegroundColor Green
-}
-# Determine if AzureAd module is already present
-$module = Get-Module -ListAvailable -Name AzureAd
-if ( -not($module)){
-  Install-Module -Name AzureAD -SkipPublisherCheck -Force -ErrorAction Stop
-  Write-Host "AzureAD module installed successfully" -ForegroundColor Green
-} else {
-  Write-Host "AzureAD module already present" -ForegroundColor Green
-  # Updating module if version 2.0.2.180
-  if ( "2.0.2.180" -eq ($module.Version.ToString())){
-    Write-Warning "Bug found! Upgrading the AzureAD module.."
-    Uninstall-Module -Name AzureAD -Force -ErrorAction Stop
-    Install-Module -Name AzureAD -SkipPublisherCheck -Force -ErrorAction Stop
-    Write-Host "AzureAD module upgraded successfully" -ForegroundColor Green
-  }
+  Write-Host "Microsoft.Graph module already present" -ForegroundColor Green
 }
 
 Connect-VB365RestorePortal -ApplicationId $applicationId
