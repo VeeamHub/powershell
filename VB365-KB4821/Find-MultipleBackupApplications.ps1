b2232b94653eafe9f527313ba26937e44836df17
Chris Arceneaux <carcenea@gmail.com>
2026-03-11 15:52:59 -0400

    Fix to remove all apps
    - Previous method left 1 auxiliary backup application that still needed to be removed.
    - Updated logic to run if ANY auxiliary backup applications are found
    
    Signed-off-by: Chris Arceneaux <carcenea@gmail.com>

--- VB365-KB4821/Find-MultipleBackupApplications.ps1
+++ VB365-KB4821/Find-MultipleBackupApplications.ps1
@@ -86,23 +86,20 @@ foreach ($org in $orgs) {
 
   # check if multiple backup applications are used
   Write-Verbose "Backup Applications found: $($apps.Count)"
-  if ($apps.Count -gt 1) {
+  if ($apps.Count -gt 0) {
     # adding field to show if backup application was removed
     $apps | Add-Member -MemberType NoteProperty -Name "RemovedFromVeeam" -Value $false
 
     # remove extra backup applications if the Fix flag is set
     if ($Fix) {
       Write-Verbose "Fix flag is set. Removing multiple backup applications for organization $($org.Name)..."
-      # $j = 1 skipping first backup application
-      $appsToRemove = $apps | Select-Object -Skip 1
-      foreach ($app in $appsToRemove) {
+      foreach ($app in $apps) {
         Write-Verbose "Removing backup application $($app.ApplicationId)"
-        Remove-VBOBackupApplication -Organization $org -BackupApplication $app -Confirm:$false
+        Remove-VBOBackupApplication -Organization $org -BackupApplication $app -Confirm:$false | Out-Null
 
         # noting that application was removed in output
         $app.RemovedFromVeeam = $true
       }
-      Clear-Variable -Name appsToRemove
     }
 
     $object = [PSCustomObject] @{
@@ -113,10 +110,6 @@ foreach ($org in $orgs) {
     Clear-Variable -Name object
   }
   Clear-Variable -Name apps
-}
-
-if ($output.Count -eq 0) {
-  Write-Verbose "No organizations with multiple backup applications found. Moving along..."
 }
 
 # logging out of Veeam session
